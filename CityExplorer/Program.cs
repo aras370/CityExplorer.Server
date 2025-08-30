8242d21f8dd6d92dcede5608970a007fb2867da7
aras370 <hesamehsani72@yahoo.com>
2025-08-30 20:45:41 +0330

    کامیت

--- CityExplorer/Program.cs
+++ CityExplorer/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson();
 
 builder.Services.AddOpenApi();
 //builder.Services.AddEndpointsApiExplorer();
